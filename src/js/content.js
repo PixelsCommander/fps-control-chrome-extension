ee98ad3a4f099e424ad21e1dfd5e849e21bf9393
PixelsCommander <dradin@evolutiongaming.com>
2018-12-30 23:24:05 +0100

    Adds FPS limit, changes icon

--- src/js/content.js
+++ src/js/content.js
@@ -1,31 +1,22 @@
 let dropFrames = 0;
-
-function alertV(s) {
-    var elt = document.createElement("script");
-    elt.innerHTML = `alert('${s}')`;
-    document.head.appendChild(elt);
-}
+let maxFPS = 0;
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     switch (message.type) {
         case 'getPopupState':
-            sendResponse({
-                type: 'setPopupState',
-                value: {
-                    dropFrames: dropFrames,
-                }
-            });
-
             chrome.runtime.sendMessage({
                 type: 'setPopupState',
                 value: {
                     dropFrames: dropFrames,
+                    maxFPS: maxFPS,
                 }
             });
-
             break;
         case 'dropFrames':
             processDropFrames(message.value);
+            break;
+        case 'limitFPS':
+            processLimitFPS(message.value);
             break;
         default:
             break;
@@ -41,23 +32,42 @@ function processDropFrames(percentage) {
         document.head.appendChild(elt);
     } else {
         var elt = document.createElement("script");
-        elt.innerHTML = `window.skipProcent = ${percentage}; window.enableDropFPS();`;
+        elt.innerHTML = ` window.setSkipProcent(${percentage}) ;window.enableVirtualRAF();`;
+        document.head.appendChild(elt);
+    }
+}
+
+function processLimitFPS(maxFPSArg) {
+    maxFPS = maxFPSArg || 0;
+
+    if (!maxFPS) {
+        var elt = document.createElement("script");
+        elt.innerHTML = `window.disableLimitFPS();`;
+        document.head.appendChild(elt);
+    } else {
+        var elt = document.createElement("script");
+        elt.innerHTML = `window.setFPSLimit(${maxFPS}) ; window.enableVirtualRAF();`;
         document.head.appendChild(elt);
     }
 }
 
 var scriptContent = `
 window.skipProcent = ${dropFrames};
+window.maxFPS = ${maxFPS};
+window.frameDelay = 0;
+
 var rafs = 0;
 
 var raf = window.requestAnimationFrame;
+var nextRAFTime = Date.now();
 
 var mockedRaf = (callback) => {
-    var skip = Math.random() < window.skipProcent;
-
+    var skip = Math.random() < window.skipProcent || Date.now() < nextRAFTime;
+    
     if (skip) {
         skippingRaf(callback);
     } else {
+        nextRAFTime = Date.now() + window.frameDelay;
         raf(callback);
     }
 
@@ -70,12 +80,29 @@ function skippingRaf(func) {
     })
 }
 
-window.enableDropFPS = function enableDropFPS() {
+window.setFPSLimit = function(v) {
+    window.frameDelay = 1000 / v;
+    window.maxFPS = v;
+}
+
+window.setSkipProcent = function(v) {
+    window.skipProcent = v; 
+}
+
+window.enableVirtualRAF = function enableDropFPS() {
     window.requestAnimationFrame = mockedRaf;
 }
 
-window.disableDropFPS = function disableDropFPS() {
+window.disableVirtualRAF = function enableDropFPS() {
     window.requestAnimationFrame = raf;
+}
+
+window.disableDropFPS = function disableDropFPS() {
+    window.skipProcent = 0;
+}
+
+window.disableLimitFPS = function disableLimitFPS() {
+    window.maxFPS = 0;
 }
 `;
 
--- src/js/popup.js
+++ src/js/popup.js
@@ -2,12 +2,17 @@ import '../css/popup.css';
 
 window.addEventListener('DOMContentLoaded', function () {
     var dropFramesSelect = document.getElementById('dropframes');
+    var limitFPSSelect = document.getElementById('limitfps');
 
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (message.type === 'setPopupState') {
             var percentage = message.value.dropFrames.toString();
             document.getElementById('dropFrames' + percentage).selected = true;
             dropFramesSelect.value = percentage;
+
+            var maxFPS = message.value.maxFPS.toString();
+            document.getElementById('limitfps' + maxFPS).selected = true;
+            limitFPSSelect.value = maxFPS;
         }
     });
 
@@ -30,6 +35,20 @@ window.addEventListener('DOMContentLoaded', function () {
         });
     };
 
+    function sendLimitFPS(e) {
+        var maxFPS = parseFloat(e.target.value);
+
+        var message = {
+            type:"limitFPS",
+            value: maxFPS,
+        };
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, message);
+        });
+    };
+
     dropFramesSelect.addEventListener('change', sendDropFrames);
+    limitFPSSelect.addEventListener('change', sendLimitFPS);
 });
 
